35557e33896b9d346998133b44be1a04c064a56e
facchin21 <ferminfacchinq@gmail.com>
2024-04-30 22:33:36 -0300

    Elimino archivo (POST.ts) que era un archivo sin uso y agrego la funcionalidad en el archivo route de login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,3 +1,71 @@
-export function POST() {
-    
+import { connectDB } from "@/libs/mongodb";
+import User, { IUser } from "@/models/User";
+import { message } from "@/utils/message";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+export async function POST(request : NextRequest) {
+    try{
+        await connectDB();
+
+        const body: IUser = await request.json();
+        const { email, password } = body;
+
+        // Validamos que se envien todos los campos
+        if(!email || !password){
+            return NextResponse.json(
+                {message: message.error.needProps},
+                {status: 400}
+            )
+        }
+        const userFind = await User.findOne({email})
+
+        // Validamos que existe el usuario por el correo
+        if(!userFind){
+            return NextResponse.json(
+                {message: message.error.userNotFound},
+                {status: 404}
+            )
+        }
+
+        const isCorrect : boolean = await bcrypt.compare(
+            password,
+            userFind.password
+        )
+        // Validamos si la contrasenia es correcta
+        if(!isCorrect){
+            return NextResponse.json(
+                {message: message.error.invalidPassword},
+            )
+        }
+
+        const { password: userPass, ...rest } = userFind._doc;
+
+        const token = jwt.sign({data: rest}, "secreto",{
+            expiresIn: 86400 // 24 horas   
+        })
+
+        const response = NextResponse.json({
+            newUser: rest,
+            message: message.success.userLogged,
+        },{
+            status: 200
+        })
+
+        response.cookies.set("auth_cookie", token, {
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 86400,
+            path: "/",
+        });
+
+        return response;
+    }catch(Error){
+        console.log('Error connecting to MongoDB');
+        return NextResponse.json(
+            {message: message.error.default},
+            {status: 500}
+        )
+    }
 }
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,3 +1,5 @@
+import { use } from "react";
+
 export const message = {
     error:{
         needProps: "Falto algun campo por rellenar",
@@ -5,8 +7,11 @@ export const message = {
         passwordNotMatch: "Las contraseñas no coinciden",
         userExist: "Ya existe un usuario con ese correo electronico",
         default: "Algo salio mal, intenta de nuevo",
+        userNotFound: "Usuario no encontrado",
+        invalidPassword: "Contraseña incorrecta",
     },
     success:{
         userCreatted: "Usuario creado con exito!",
+        userLogged : "Usuario logeado con exito!",
     }
 }
